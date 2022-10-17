647abad1ce84aeb581be2e35410b3c1e30894ae1
Joaquingse <joaquingse@gmail.com>
2022-10-17 13:40:41 +0100

    our game is a dream.... or not 2

--- script/script.js
+++ script/script.js
@@ -3,15 +3,28 @@ var direction = 1
 var left = 15
 var table = document.getElementById('table');
 var button = document.getElementById('button')
+var spaceShip = document.getElementById('spaceShip')
 var timerId;
+
+spaceShip = {
+    x:285,
+    y:560,
+    dir: 'left'
+}
+
+
 
 var moveAliens = function (){
 
 }
 
 function moveSpaceShip () {
-
-
+    if(spaceShip.dir === 'left'){
+        spaceShip.x--
+    }
+    if(spaceShip.dir === 'rigth'){
+        spaceShip.x++
+    }
 }
 
 
