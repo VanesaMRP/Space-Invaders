cb0ab4d2feaab01c1482a9ff559331c2ff504745
Joaquingse <joaquingse@gmail.com>
2022-10-18 09:51:12 +0100

    shooting aliens

--- script/script.js
+++ script/script.js
@@ -8,8 +8,8 @@ var timerId;
 var shoot = document.getElementsByClassName('shoot')
 
 var space = {
-    x:10,
-    y:16,
+    x: 10,
+    y: 16,
     direction: 'left',
 }
 var shootSpace = {
@@ -17,72 +17,90 @@ var shootSpace = {
     y: 15,
     direction: 'up',
 }
-function drawShip(){
-    var shipCell= document.querySelector(`.row${space.y} .col${space.x}`)
+function drawShip() {
+    var shipCell = document.querySelector(`.row${space.y} .col${space.x}`)
     console.log(shipCell)
-shipCell.classList.add('spaceShip') 
+    shipCell.classList.add('spaceShip')
 }
 
-function removeShip(){
-    var shipCell2= document.querySelector('.spaceShip')
+function removeShip() {
+    var shipCell2 = document.querySelector('.spaceShip')
     console.log(shipCell2)
     shipCell2.classList.remove('spaceShip')
 }
 
-function moveSpaceShip () {
-    if(space.direction === 'left' && space.x > 1){
-        space.x --
+function moveSpaceShip() {
+    if (space.direction === 'left' && space.x > 1) {
+        space.x--
+        //shootSpace.x--
         console.log('left')
     }
-    if(space.direction === 'rigth'&& space.x < 19){
-        space.x ++
+    if (space.direction === 'rigth' && space.x < 19) {
+        space.x++
+        //shootSpace.x++
         console.log('right')
-    } 
     }
-    function shooter (){
-        var shootCell =  document.querySelector(`.row${space.y} .col${space.x}`)
-        console.log(shootCell)
-        shootCell.classList.add('shoot')
+}
+function drawShoot() {
+    var shootCell = document.querySelector(`.row${shootSpace.y} .col${shootSpace.x}`)
+    console.log(shootCell)
+    shootCell.classList.add('shoot')
+}
+    var moveShoot = function() {
+    if (shootSpace.direction === 'up' && shootSpace.y > 1) {
+        drawShoot()
+        removeShoot()
+        shootSpace.y--
+        drawShoot()
     }
-    function moveShoot(){
-        if(shootSpace.direction === 'up'){
-            shootSpace.y --
-        }
-    }
-window.addEventListener('keydown', function(e){
-    switch (e.code){
+}
+
+function removeShoot (){
+    var shootCell2 = document.querySelector('.shoot')
+    shootCell2.classList.remove('shoot')
+}
+
+window.addEventListener('keydown', function (e) {
+    switch (e.code) {
         case 'ArrowLeft':
-        space.direction = 'left'
-        removeShip()
-        moveSpaceShip()
-        drawShip()
-        break 
+            space.direction = 'left'
+            removeShip()
+            moveSpaceShip()
+            drawShip()
+            break
         case 'ArrowRight':
-        space.direction = 'rigth'
-        removeShip()
-        moveSpaceShip()
-        drawShip()
-        break
+            space.direction = 'rigth'
+            removeShip()
+            moveSpaceShip()
+            drawShip()
+            break
+        case 'ArrowUp':
+            shootSpace.direction = 'up'
+            
+            timerId = setInterval(moveShoot ,500)
+            //removeShoot()
+           // drawShoot()
+            break
         case 'keyup':
-        space.direction = 0
-        break
-        case 'spaceBar':
-        shoot.direction = 'up'
+            space.direction = 0
+            shootSpace.direction = 0
+            break
+
     }
- 
+
 })
 
 
 
-var moveAliens = function (){
-    if(left >= 125 || left < 15) { direction *= -1}
+var moveAliens = function () {
+    if (left >= 125 || left < 15) { direction *= -1 }
     left += 10 * direction
     table.style.left = left + 'px'
 }
 
 
-button.addEventListener('click', function (e){
-    if (!moving){
+button.addEventListener('click', function (e) {
+    if (!moving) {
         timerId = setInterval(moveAliens, 500)
     } else {
         clearInterval(timerId)
