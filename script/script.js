7141293846f8f766b2beb17ae517ed1863051bdf
Fer Martin <fer@reboot.academy>
2022-10-19 10:05:37 +0100

    aliens as array, move with interval

--- script/script.js
+++ script/script.js
@@ -1,169 +1,167 @@
-var moving = false
-var direction = 1
-var button = document.getElementById('button')
 var spaceShip = document.querySelector('.spaceShip')
 var timerId;
 var timerId3;
 var shoot = document.getElementsByClassName('shoot')
 
-var alien = {
-    x: 1,
-    y: 1,
-    direction: 1
+var aliens =  {
+  direction: 1, // 1-derecha, 0-izquierda
+  naves: [
+    { x: 4, y: 1 },
+    { x: 8, y: 1 },
+    { x: 12, y: 1 },
+    { x: 16, y: 1 },
+  ],
+  remove: function() {
+    document
+      .querySelectorAll('.aliens')
+      .forEach(alien => {
+        alien.classList.remove('aliens')
+      })
+  },
+  draw: function() {
+    this.naves.forEach(nave => {
+      var alienCell = document.querySelector(`.row${nave.y} > .col${nave.x}`)
+      alienCell.classList.add('aliens')
+    })
+  },
+  move: function() {
+    this.naves.forEach(nave => {
+      nave.x++
+    })
+  }
 }
 
 var space = {
-    x: 10,
-    y: 16,
-    direction: null,
+  x: 11,
+  y: 16,
+  draw: function () {
+    var shipCell = document.querySelector(`.row${space.y} > .col${space.x}`)
+    shipCell.classList.add('spaceShip')
+  },
+  remove: function () {
+    var shipCell2 = document.querySelector('.spaceShip')
+    shipCell2.classList.remove('spaceShip')
+  }
 }
 
 var shootSpace = {
-    x: 0,
-    y: 0
+  x: 0,
+  y: 0
 }
 // mover la nave
 
-function drawShip() {
-    var shipCell = document.querySelector(`.row${space.y} .col${space.x}`)
-    shipCell.classList.add('spaceShip')
-}
-
-function removeShip() {
-    var shipCell2 = document.querySelector('.spaceShip')
-    shipCell2.classList.remove('spaceShip')
-}
-
-function moveSpaceShip() {
-    if (space.direction === 'left' && space.x > 1) {
-        space.x--
-    }
-    if (space.direction === 'right' && space.x < 19) {
-        space.x++
-    }
-}
-
 // disparar
-
 function drawShoot() {
-    var shootCell = document.querySelector(`.row${shootSpace.y} .col${shootSpace.x}`)
-    shootCell.classList.add('shoot')
+  var shootCell = document.querySelector(`.row${shootSpace.y} .col${shootSpace.x}`)
+  shootCell.classList.add('shoot')
 }
 
 function removeShoot() {
-    var shootCell = document.querySelector('.shoot')
-    shootCell.classList.remove('shoot')
+  var shootCell = document.querySelector('.shoot')
+  shootCell.classList.remove('shoot')
 }
 
 function moveShoot() {
-    if (shootSpace.y === 0) {
-        shootSpace.y = null
-        shootSpace.x = null
-        //clearInterval(timerId3)
-    } else {
-        drawShoot()
-        removeShoot()
-        shootSpace.y--
-        drawShoot()
-    }
+  if (shootSpace.y === 0) {
+    shootSpace.y = null
+    shootSpace.x = null
+    //clearInterval(timerId3)
+  } else {
+    drawShoot()
+    removeShoot()
+    shootSpace.y--
+    drawShoot()
+  }
 }
 
 // collision and explode
 
 function checkHit() {
-    var shootCell = document.querySelector('.shoot')
-    if (shootCell.classList.contains('aliens')) {
-        shootCell.classList.remove('aliens')
-        shootCell.classList.remove('shoot')
-        shootSpace.y = null
-        shootSpace.x = null
-    }
+  var shootCell = document.querySelector('.shoot')
+  if (shootCell.classList.contains('aliens')) {
+    shootCell.classList.remove('aliens')
+    shootCell.classList.remove('shoot')
+    shootSpace.y = null
+    shootSpace.x = null
+  }
 }
 
 window.addEventListener('keydown', function (e) {
-    switch (e.code) {
-        case 'ArrowLeft':
-            space.direction = 'left'
-            break
-        case 'ArrowRight':
-            space.direction = 'right'
-            break
-        case 'ArrowUp':
-            if (shootSpace.x === null && shootSpace.y === null) {
-                shootSpace.x = space.x
-                shootSpace.y = space.y - 1
-            }
-            break
-    }
+  switch (e.code) {
+    case 'ArrowLeft':
+      if (space.x > 1) { space.x-- }
+      break;
+
+    case 'ArrowRight':
+      if (space.x < 20) { space.x++  }
+      break;
+
+    case 'ArrowUp':
+      if (shootSpace.x === null && shootSpace.y === null) {
+        shootSpace.x = space.x
+        shootSpace.y = space.y - 1
+      }
+      break
+  }
 
 })
-
-window.addEventListener('keyup', function (e) {
-    if (e.code === 'ArrowLeft' || e.code === 'ArrowRight') {
-        space.direction = null
-    }
-})
-
-
-// mover aliens
-
-function alienMovement() {
-    timerId = setInterval(function () {
-        //devuelve array
-        var alienCell = document.querySelectorAll(`.aliens`)
-        console.log(alienCell)
-        alienCell.forEach(function (et) {
-            et.classList.remove('aliens')
-            var row = et.parentNode.getAttribute('class')
-            var col = et.getAttribute('class')
-            var cel = document.querySelector(`.row${parseInt(row[row.length-1]+1)} .${col}`)
-            cel.classList.add('aliens')
-            console.log(row, col, cel, row.length)
-        })
-
-
-    }, 1000)
-}
-
 
 // que comience el juego
 
 function startGame() {
 
-    var gameTimer = setInterval(function () {
-        removeShip()
-        moveSpaceShip()
-        drawShip()
-    }, 200)
-    var gameShoot = setInterval(function () {
-        moveShoot()
-        checkHit()
-    }, 200)
+  space.draw()
+  aliens.draw()
+
+  // Move SpaceShip
+  var gameTimer = setInterval(function () {
+    space.remove()
+    space.draw()
+  }, 50)
+
+  // Move Aliens
+  var aliensTimer = setInterval(function () {
+    aliens.remove()
+    aliens.move()
+    aliens.draw()
+  }, 200)
+
+  // Move SpaceShip Shoot
+  var gameShoot = setInterval(function () {
+    moveShoot()
+    checkHit()
+  }, 200)
 
 }
+
+
 const TABLE_WIDTH = 20
 const TABLE_HEIGHT = 16
 
 function createTable() {
   const table = document.getElementById('table')
 
-  for (let i=0; i<TABLE_HEIGHT; i++)  {
+  for (let i = 0; i < TABLE_HEIGHT; i++) {
     const tr = document.createElement('tr')
-    tr.classList.add(`row${i+1}`)
-    for (let j=0; j<TABLE_WIDTH; j++)  {
+    tr.classList.add(`row${i + 1}`)
+    for (let j = 0; j < TABLE_WIDTH; j++) {
       const td = document.createElement('td')
-      td.classList.add(`col${j+1}`)
+      td.classList.add(`col${j + 1}`)
       tr.appendChild(td)
     }
     table.appendChild(tr)
   }
 }
-createTable()
 
-button.addEventListener('click', function (e) {
-    startGame()
-    alienMovement()
-})
+function init() {
+  createTable()
+  document
+    .getElementById('start')
+    .addEventListener('click', () => startGame() )
+}
+
+init()
+
 
 
 
